b14c2ac1e5975af54d6bb3b53035712c042ab90d
Maggie <mengjing0818@gmail.com>
2021-06-16 12:00:01 -0700

    Used Geolocation to get user's location and refactored from Functional to Class Components.

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,8 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-const App = () => {
-  return <div>Hi there!</div>;
-};
 
-ReactDOM.render(<App />, document.querySelector());
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => console.log(position),
+      err => console.log(err)
+    );
+
+    return <div>Latitude: </div>
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
