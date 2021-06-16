127828a486a74126e43e757ebeb49b61bd1550bc
Maggie <mengjing0818@gmail.com>
2021-06-15 17:09:21 -0700

    Props system.

--- components/src/index.js
+++ components/src/index.js
@@ -6,11 +6,9 @@ import CommentDetail from './CommentDetail';
 const App = () => {
     return (
         <div className="ui container comments">
-           <CommentDetail />
-           <CommentDetail />
-           <CommentDetail />
-           <CommentDetail />
-           <CommentDetail />
+            <CommentDetail author="Sam" timeAgo="Today at 4:45PM" content="Nice blog post!" avatar={faker.image.avatar()} />
+            <CommentDetail author="Alex" timeAgo="Today at 2:00AM" content="Good!" avatar={faker.image.avatar()} />
+            <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" content="Like it!" avatar={faker.image.avatar()} />
         </div>
     );
 };
