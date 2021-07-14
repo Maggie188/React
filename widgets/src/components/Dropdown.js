edd6d4b1400f4ae1dd8c5c900d7f0147c6d6613b
Maggie <mengjing0818@gmail.com>
2021-07-13 22:55:49 -0700

    The translate widget.

--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 
-const Dropdown = ({ options, selected, onSelectedChange }) => {
+const Dropdown = ({ label, options, selected, onSelectedChange }) => {
     const [open, setOpen] = useState(false);
     const ref = useRef();
 
@@ -14,7 +14,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
         document.body.addEventListener('click', onBodyClick, { capture: true });
 
         return () => {
-            document.body.removeEventListener('click', onBodyClick);
+            document.body.removeEventListener('click', onBodyClick, { capture: true });
         };
     }, []);
 
@@ -37,7 +37,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
     return (
         <div ref={ref} className="ui form">
             <div className="field">
-                <label className="label">Select a Color</label>
+                <label className="label">{label}</label>
                 <div 
                     onClick={() => setOpen(!open)}
                     className={`ui selection dropdown ${open? 'visible active' : ''}`}
