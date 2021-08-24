ce548ab93d28e49d14db5bde147f3466ff1627f3
Maggie <mengjing0818@gmail.com>
2021-08-23 17:09:49 -0700

    Refactor stream creation.

--- streams/client/src/components/streams/StreamEdit.js
+++ streams/client/src/components/streams/StreamEdit.js
@@ -6,10 +6,18 @@ class StreamEdit extends React.Component {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id)
   }
+
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+
+    return <div>{this.props.stream.title}</div>;
+  }
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] }
 };
 
-export default connect(mapStateToProps, { fecthStream })(StreamEdit);
+export default connect(mapStateToProps, { fetchStream })(StreamEdit);
